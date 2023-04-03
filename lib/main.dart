c9ad79013c6672dd23fba6f22c6321adfa08ce77
Lucas Claros <lucas.claros11@gmail.com>
2023-04-03 05:00:41 -0300

    Add method handler for url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:mepaga_ai/common/general_provider.dart';
 import 'package:mepaga_ai/common/routing.dart';
+import 'package:mepaga_ai/url_strategy/nonweb_url_strategy.dart'
+    if (dart.library.html) 'package:mepaga_ai/url_strategy/web_url_strategy.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (kIsWeb) {
-    setUrlStrategy(PathUrlStrategy());
-  }
+  configureUrl();
 
   runApp(
     const GeneralProvider(
